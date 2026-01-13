09ca11ed053f677eadf476ab7675e0c88453bbe9
Z-Graham <2260220@flourbluffschools.org>
2026-01-13 12:11:58 -0600

    particles

--- player/player.gd
+++ player/player.gd
@@ -3,28 +3,39 @@ class_name Player
 
 @export var force=1500.0
 @export var turn_force=600.0
+@onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
 
 var transitioning=false
 func _process(delta: float) -> void:
 	if not transitioning:
 		if Input.is_action_pressed("boost"):
-			if not $RocketAudio.is_playing():
-				$RocketAudio.play()
-			else:
-				$RocketAudio.stop()
 			apply_central_force(basis.y*delta*force)
+			$mainBooster.emitting=true
+			if not rocket_audio.is_playing():
+				rocket_audio.play()
+		else:
+			rocket_audio.stop()
+			$mainBooster.emitting=false
 			
 		if Input.is_action_pressed("rotate_left"):
 			apply_torque(Vector3(0.0,0.0,1.0)*delta*turn_force)
+			$rightBooster.emitting=true
+		else:
+			$rightBooster.emitting=false
 			
 		if Input.is_action_pressed("rotate_Right"):
 			apply_torque(Vector3(0.0,0.0,-1.0)*delta*turn_force)
+			$leftBooster.emitting=true
+		else:
+			$leftBooster.emitting=false
 		
 
 func crash_sequence():
 	$explosionAudio.play()
-	gravity_scale=0
 	transitioning=true
+	$mainBooster.emitting=false
+	$rightBooster.emitting=false
+	$leftBooster.emitting=false
 	await get_tree().create_timer(2.5).timeout
 	get_tree().reload_current_scene.call_deferred()
 
